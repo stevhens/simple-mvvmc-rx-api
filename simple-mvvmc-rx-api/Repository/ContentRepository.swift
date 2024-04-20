fc593e218279dc96ae032410fd430a7c564dfb2c
Stevhen <stevhen@snappymob.com>
2024-04-20 19:18:20 +0700

    Add Repository to fetch Joke detail

--- simple-mvvmc-rx-api/Repository/ContentRepository.swift
+++ simple-mvvmc-rx-api/Repository/ContentRepository.swift
@@ -2,4 +2,5 @@ import RxSwift
 
 protocol ContentRepository {
     func fetchJokeList() -> Observable<[Joke]>
+    func fetchJoke(id: Int) -> Observable<Joke>
 }
--- simple-mvvmc-rx-api/Repository/DefaultContentRepository.swift
+++ simple-mvvmc-rx-api/Repository/DefaultContentRepository.swift
@@ -11,4 +11,9 @@ class DefaultContentRepository: ContentRepository {
         let url = "https://official-joke-api.appspot.com/jokes/ten"
         return networkClient.fetchData(from: url)
     }
+    
+    func fetchJoke(id: Int) -> Observable<Joke> {
+        let url = "https://official-joke-api.appspot.com/jokes/\(id)"
+        return networkClient.fetchData(from: url)
+    }
 }
